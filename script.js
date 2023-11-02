9d7d6b3518c68a54ccbcdb6d96923c8b0c9a7ceb
Redegit <dima070603@gmail.com>
2023-11-02 19:05:15 +0300

    Добавлена линия при завершении игры

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
+class Cell {
+    static cellList = [];
+
+    constructor(row_i, col_i, htmlElement) {
+        this.row_i = row_i
+        this.col_i = col_i
+        this.element = htmlElement
+        Cell.cellList.push(this)
+    }
+
+    static findByCoord(row_i, col_i) {
+        return Cell.cellList.filter((cell) =>
+            cell.row_i === row_i && cell.col_i === col_i
+        )[0]
+    }
+
+    static clearCellList() {
+        Cell.cellList = []
+    }
+
+    static createGameField() {
+        const fieldElement = document.getElementById("field");
+        fieldElement.innerHTML = "";
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement("div");
+                cell.className = "cell";
+                cell.dataset.value = field[i][j];
+                cell.dataset.col = j;
+                cell.dataset.row = i;
+                cell.addEventListener("click", () => onCellClick(i, j));
+                fieldElement.appendChild(cell);
+                new Cell(i, j, cell);
+            }
+        }
+    }
+
+    static resetGameField() {
+        Cell.clearCellList()
+        Cell.createGameField()
+    }
+}
+
+
 // Функция для проверки, кто выиграл или ничья в игре
 function checkWin() {
     // Проверка выигрыша по горизонтали (по строкам)
-    for (let row of field) {
-        if (row[0] === row[1] && row[1] === row[2] && row[0] !== "-") {
-            return row[0];
+    for (let row_i = 0; row_i < 3; row_i++) {
+        if (field[row_i][0] === field[row_i][1] && field[row_i][1] === field[row_i][2] && field[row_i][0] !== "-") {
+            return { winner: field[row_i][0], dim: "row", index: row_i };
         }
     }
 
     // Проверка выигрыша по вертикали (по столбцам)
-    for (let x = 0; x < 3; x++) {
-        if (field[0][x] === field[1][x] && field[1][x] === field[2][x] && field[0][x] !== "-") {
-            return field[0][x];
+    for (let col_i = 0; col_i < 3; col_i++) {
+        if (field[0][col_i] === field[1][col_i] && field[1][col_i] === field[2][col_i] && field[0][col_i] !== "-") {
+            return { winner: field[0][col_i], dim: "col", index: col_i };
         }
     }
 
     // Проверка выигрыша по диагонали (слева направо)
     if (field[0][0] === field[1][1] && field[1][1] === field[2][2] && field[0][0] !== "-") {
-        return field[0][0];
+        return { winner: field[0][0], dim: "diag", index: 0 };
     }
 
     // Проверка выигрыша по диагонали (справа налево)
     if (field[0][2] === field[1][1] && field[1][1] === field[2][0] && field[0][2] !== "-") {
-        return field[0][2];
+        return { winner: field[0][2], dim: "diag", index: 1 };
     }
 
     // Подсчет количества пустых ячеек
@@ -36,11 +80,11 @@ function checkWin() {
 
     // Если все ячейки заняты и нет выигрыша, то ничья
     if (countEmpty === 0) {
-        return "tie";
+        return { winner: "tie" };
     }
 
-    // Если игра продолжается, возвращаем false
-    return false;
+    // Если игра продолжается, возвращаем null
+    return { winner: null };
 }
 
 // Инициализация игрового поля
@@ -67,32 +111,7 @@ function updateStatus(text) {
     statusElement.textContent = text;
 }
 
-// Получение элемента для отображения игрового поля
-const fieldElement = document.getElementById("field");
-
-// Функция для обновления пользовательского интерфейса (игрового поля)
-function updateFieldUI() {
-    fieldElement.innerHTML = '';
-
-    if (turn === 'x') {
-        updateStatus("Ходит крестик");
-    } else {
-        updateStatus("Ходит нолик");
-    }
-
-    // Создание и добавление ячеек в игровое поле
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            const cell = document.createElement("div");
-            cell.className = "cell";
-            cell.dataset.value = field[i][j];
-            cell.dataset.col = j;
-            cell.dataset.row = i;
-            cell.addEventListener("click", () => onCellClick(i, j));
-            fieldElement.appendChild(cell);
-        }
-    }
-}
+Cell.createGameField();
 
 // Инициализация текущего игрока (крестик начинает)
 let turn = "x";
@@ -100,49 +119,146 @@ let turn = "x";
 // Флаг, указывающий на завершение игры
 let gameEnd = false;
 
-// Обновление интерфейса при загрузке страницы
-updateFieldUI();
-
 // Функция, вызываемая при клике на ячейку
 function onCellClick(y, x) {
-    if (gameEnd) return; // Если игра завершена, игрок не может делать ход
+    if (gameEnd) { startNewGame(); return; } // Если игра завершена, игрок не может делать ход
     if (field[y][x] !== "-") {
-        console.log("Тут уже занято");
         return;
     }
 
     // Устанавливаем значение в выбранную ячейку (ход текущего игрока)
     field[y][x] = turn;
+
+    // Обновление ячейки
+    Cell.findByCoord(y, x).element.dataset.value = turn;
+
     // Смена текущего игрока
     turn = (turn === 'x') ? 'o' : 'x';
 
-    // Обновление интерфейса
-    updateFieldUI();
+    // Обновление статуса
+    if (turn === 'x') {
+        updateStatus("Ходит крестик");
+    } else {
+        updateStatus("Ходит нолик");
+    }
 
     // Проверка, завершена ли игра
     let result = checkWin();
-    if (result) {
-        if (result === "tie") {
-            updateStatus("Ничья, попробуйте еще раз");
-        } else {
-            if (result === "x") {
-                updateStatus("Поздравляем! Победил крестик");
-            } else {
-                updateStatus("Поздравляем! Победил нолик");
-            }
-        }
+
+    function gameResult(status) {
         gameEnd = true;
-        return;
+        updateStatus(status)
+        drawLine(result)
+    }
+
+    switch (result.winner) {
+        case "tie":
+            gameResult("Ничья, попробуйте еще раз");
+            break;
+        case "x":
+            gameResult("Поздравляем! Победил крестик");
+            break;
+        case "o":
+            gameResult("Поздравляем! Победил нолик");
+            break;
+        case null: break;
+        default: break;
+    }
+}
+
+function drawLine({ dim, index }) {
+    function createLine(cell1, cell2, rotation_deg) {
+        const line = document.createElement("line");
+
+        line.className = "line";
+
+        line_padding = 3; // in rem
+        x_shift = y_shift = 0;
+        switch (rotation_deg) {
+            case "90deg":
+                y_shift -= line_padding / 2;
+                break;
+            case "0deg":
+                x_shift -= line_padding / 2;
+                break;
+            case "45deg":
+                x_shift -= line_padding / 2 / Math.sqrt(2);
+                y_shift -= line_padding / 2 / Math.sqrt(2);
+                break;
+            case "135deg":
+                x_shift += line_padding / 2 - Math.sqrt(2);
+                y_shift -= line_padding / 2 - Math.sqrt(2);
+                break;
+        }
+
+        cell1_center = getCellCenter(cell1.element);
+        cell2_center = getCellCenter(cell2.element);
+
+        x_dif = cell1_center.center_x - cell2_center.center_x
+        y_dif = cell1_center.center_y - cell2_center.center_y
+        line_width = Math.sqrt(x_dif ** 2 + y_dif ** 2)
+
+        line.style.cssText = `
+            --width: calc(${line_width}px + ${line_padding}rem); 
+            --left: calc(${cell1_center.center_x}px + ${x_shift}rem); 
+            --top: calc(${cell1_center.center_y}px + ${y_shift}rem);
+            rotate: ${rotation_deg}`
+        const field = document.getElementById("field");
+        field.appendChild(line);
+    }
+
+    function getCellCenter(cell) {
+        console.log(cell.getBoundingClientRect());
+        let { x, y, height, width } = cell.getBoundingClientRect();
+        center_x = x + width / 2;
+        center_y = y + height / 2;
+        return { center_x, center_y }
+    }
+
+    switch (dim) {
+        case "col":
+            createLine(
+                Cell.findByCoord(0, index),
+                Cell.findByCoord(2, index),
+                "90deg"
+            );
+            break;
+        case "row":
+            createLine(
+                Cell.findByCoord(index, 0),
+                Cell.findByCoord(index, 2),
+                "0deg"
+            );
+            break;
+        case "diag":
+            if (index === 0) {
+                createLine(
+                    Cell.findByCoord(0, 0),
+                    Cell.findByCoord(2, 2),
+                    "45deg"
+                );
+            } else {
+                createLine(
+                    Cell.findByCoord(0, 2),
+                    Cell.findByCoord(2, 0),
+                    "135deg"
+                );
+            }
+            break;
     }
 }
 
 // Получение кнопки для перезапуска игры
 const restartButton = document.getElementById("btn-restart");
 
-// Обработчик события для кнопки перезапуска игры
-restartButton.addEventListener("click", () => {
+function startNewGame() {
     gameEnd = false;
     resetField();
+    Cell.resetGameField();
     turn = 'x';
-    updateFieldUI();
+    updateStatus("Ходит крестик");
+}
+// Обработчик события для кнопки перезапуска игры
+restartButton.addEventListener("click", () => {
+    startNewGame();
 });
